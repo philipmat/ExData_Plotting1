4c6f901c8fc9f1e62005d691f80e808320be5da6
Philip Mateescu <hello@philipm.at>
2015-09-10 23:09:35 -0500

    improved the run method and added some comments

--- plot1.R
+++ plot1.R
@@ -1,4 +1,11 @@
 # Plot 1 - Global active power
+# The script will execute if you simply source('plot1.R')
+# if you want to use it as a module (for example see README.Rmd)
+# first set `options(plot1.run=F)`, which will disable the
+# automatic running of the functions
+
+# reads data from a file, optionally subsetting it
+# Eg: readData('data/power.txt', c('1/2/2007', '2/2/2007'))
 
 readData <- function(fname, date.subset = NA) {
     if (!file.exists(fname)) {
@@ -11,6 +18,9 @@ readData <- function(fname, date.subset = NA) {
     }
     return(power.data)
 }
+
+# plot the Global_active_power as a histogram to outFile
+# set outFile to NA to plot to screen instead
 
 plotData <- function(data, outFile = 'plot1.png') {
     hist(
@@ -26,15 +36,16 @@ plotData <- function(data, outFile = 'plot1.png') {
     }
 }
 
-if (getOption('plot1.test', default=F)) {
-    dfile <- './data/short_power.txt'
-    powdata <- readData(dfile, date.subset = c('1/2/2007', '2/2/2007'))
-    print(summary(powdata))
-    plotData(powdata, outFile = NA)
-}
 if (getOption('plot1.run', default=T)) {
-    dfile <- './data/short_power.txt'
-    powdata <- readData(dfile)
-    plotData(powdata, outFile = NA)
-    ## print(summary(powdata))
+    dfile <- './data/household_power_consumption.txt'
+    outFile <- 'plot1.png'
+    isTest <- getOption('plot1.test', default=F)
+    if (isTest) {
+        dfile <- './data/short_power.txt' # short_power is a smaller version
+        outFile <- NA  # NA = plot to screen
+    }
+
+    powdata <- readData(dfile, date.subset = c('1/2/2007', '2/2/2007'))
+    plotData(powdata, outFile = outFile)
+    if (isTest) { print(summary(powdata)) }
 }
